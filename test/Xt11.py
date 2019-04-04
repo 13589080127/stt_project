fcc810cfee2514b36f410dc85bd477cbc257d2c8
songtiantian <1599798497@qq.com>
2019-04-04 18:00:20 +0800

    meiyou

---
+++ test/Xt11.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8
+
+# 提问
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weighe?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+
+
+
+
+
+
+
+
+
+
+print "Please input a num:"
+k = int(raw_input())
+print k
+print type(k)
