f607218b87bba256edab1c2ab9e74b4fcd5c9370
songtiantian <1599798497@qq.com>
2019-04-11 17:50:59 +0800

    meiyou

---
+++ test/Xt16.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# 读写文件
+
+
+#= = 和true、false如何使用 
+#
+#习题4，常见问题回答，print时词语间的空格……
+#
+#习题5，常见问题回答，浮点数四舍五入，round（1.773）
+
+
+
+from sys import  argv
+
+script, filename = argv
+
+print "We're going to erase %r." % filename
+print "If you don't want that, hit CTRL-C (^C)."
+print "If you do want that, hit RETURN."
+
+raw_input("?")
+
+print "Opening the file..."
+target = open(filename, 'w')
+
+print "Truncating the file. Goodbye!"
+target.truncate()
+
+print "Now I'm going to ask you for three lines."
+
+linel = raw_input("line 1: ")
+line2 = raw_input("line 2: ")
+line3 = raw_input("line 3: ")
+
+print "I'm going to write these to the file."
+
+targte.write(line1)
+targte.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.weite("\n")
+
+print "And finally, we close it."
+target.close()
